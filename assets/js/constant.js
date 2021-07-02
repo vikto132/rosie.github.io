24b90aeb7b1fade9ff71d95ac59aa5a293bb1540
Ly Tung <vikto132@gmail.com>
2021-07-02 18:10:40 +0700

    done animation

--- assets/js/constant.js
+++ assets/js/constant.js
@@ -2,10 +2,13 @@ var pictureSilder = {
     'childhood':[
         '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','cover.jpg'
     ],
-    'midleshcool':[
+    'midleschool':[
         '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg'
     ],
     'highschool':[
         '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','cover.jpg'
+    ],
+    'university':[
+        '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','cover.jpg'
     ]
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
+$(function(){
+  $(window).scroll(function(){
+    console.log($(window).scrollTop());
+    const height = $(window).scrollTop();
+    if(height > 790){
+      const element = document.getElementsByClassName('milestone');
+      Array.from(element).forEach(element => {
+        element.classList.add('milestone-animation');
+        setTimeout(() => {
+          element.children[0].classList.add('milestone__image-animate');
+        }, 1000);
+        setTimeout(() => {
+          element.children[1].classList.add('milestone__content-animate');
+        }, 1000*2);
+      });
+    }
+    if(height > 2356){
+      const element = document.getElementsByClassName('pf');
+      Array.from(element).forEach(element =>{
+        element.classList.add('animate');
+      })
+    }
   });
+});
